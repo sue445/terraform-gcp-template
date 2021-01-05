a7e655062a7cedce508703d9a779e511de66f99e
sue445 <sue445@sue445.net>
2021-01-06 00:03:45 +0900

    Write doc

--- versions.tf
+++ versions.tf
@@ -4,13 +4,14 @@ terraform {
       source = "hashicorp/google"
 
       # c.f. https://github.com/hashicorp/terraform-provider-google/blob/master/CHANGELOG.md
-      version = "3.51.0"
+      version = "3.51.0" # Edit here
     }
     google-beta = {
       source = "hashicorp/google-beta"
+
       # c.f. https://github.com/hashicorp/terraform-provider-google-beta/blob/master/CHANGELOG.md
-      version = "3.51.0"
+      version = "3.51.0" # Edit here
     }
   }
-  required_version = ">= 0.14"
+  required_version = ">= 0.14" # Edit here
 }
