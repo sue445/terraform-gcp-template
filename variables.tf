82d14411ef3cc3363af8c471756589d3b9ddba25
sue445 <sue445@sue445.net>
2021-12-26 20:34:32 +0900

    Tweak comment

--- variables.tf
+++ variables.tf
@@ -5,13 +5,13 @@ variable "gcp_project_id" {
 
 variable "provider_region" {
   type        = string
-  description = "provider region (see. https://cloud.google.com/compute/docs/regions-zones)"
+  description = "Provider region (see. https://cloud.google.com/compute/docs/regions-zones)"
 }
 
 # NOTE: available format are https://registry.terraform.io/providers/hashicorp/google/latest/docs/data-sources/service_account#account_id
 variable "terraform_service_account_id" {
   type        = string
-  description = "account id for the service account used by GitHub Actions"
+  description = "Account ID for the service account used by GitHub Actions"
 }
 
 variable "github_username" {
@@ -21,5 +21,5 @@ variable "github_username" {
 
 variable "github_repository" {
   type        = string
-  description = "GitHub repository name (e.g. my-terraform)"
+  description = "GitHub repository name (e.g. my-terraform-repo)"
 }
