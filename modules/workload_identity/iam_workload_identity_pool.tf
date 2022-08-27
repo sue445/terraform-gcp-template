0ec2179644a15596df4da99a0a3d6c633f448ccb
sue445 <sue445@sue445.net>
2022-08-27 16:32:30 +0900

    Add workload_identity module

---
+++ modules/workload_identity/iam_workload_identity_pool.tf
@@ -0,0 +1,31 @@
+# c.f. https://cloud.google.com/iam/docs/configuring-workload-identity-federation#github-actions
+
+resource "google_iam_workload_identity_pool" "github_actions" {
+  provider = google-beta
+
+  workload_identity_pool_id = var.workload_identity_pool_id
+}
+
+resource "google_iam_workload_identity_pool_provider" "github_actions" {
+  provider = google-beta
+
+  workload_identity_pool_id          = google_iam_workload_identity_pool.github_actions.workload_identity_pool_id
+  workload_identity_pool_provider_id = "github-actions"
+
+  attribute_mapping = {
+    "google.subject"       = "assertion.sub"
+    "attribute.repository" = "assertion.repository"
+  }
+
+  oidc {
+    issuer_uri = "https://token.actions.githubusercontent.com"
+  }
+}
+
+resource "google_service_account_iam_member" "link_to_repo" {
+  for_each = toset(var.service_account_names)
+
+  service_account_id = each.value
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_actions.name}/attribute.repository/${var.github_username}/${var.github_repository}"
+}
